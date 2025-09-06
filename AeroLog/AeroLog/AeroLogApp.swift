99a01d1b6798176a9c26f99024c2a5eea09093ac
yuhan0113 <2856528583@qq.com>
2025-09-06 23:48:18 +1000

    initial setup

---
+++ AeroLog/AeroLog/AeroLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroLogApp.swift
+//  AeroLog
+//
+//  Created by 張宇漢 on 6/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct AeroLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
